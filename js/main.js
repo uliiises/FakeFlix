e41d5249525ac213374189295da6bcd31fd012dc
UlisesPA <ulises-96_@hotmail.com>
2020-07-25 21:49:11 -0500

    Se utiliza Js para programar las flechas y se agregan los botones de paginacion

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const fila = document.querySelector('.contenedor_carousel');
-const peliculas = document.querySelector('.pelicula');
+const peliculas = document.querySelectorAll('.pelicula');
 
 const flecha_izq = document.getElementById('flecha_izq');
 const flecha_der = document.getElementById('flecha_der');
@@ -15,3 +15,14 @@ flecha_der.addEventListener('click', () => {
 flecha_izq.addEventListener('click', () => {
 	fila.scrollLeft -= fila.offsetWidth
 });
+
+/*------------------- PAGINACION -------------------*/
+/*Math.ceil -> METODO PARA REDONDEAR EL RESULTADO*/
+const numeroPaginas = Math.ceil(peliculas.length/5);
+for (let i = 0; i < numeroPaginas; i++) { /*CONDICION DE NUMERO DE PAGINAS*/
+	const indicador = document.createElement('button'); /*CREACION DE BOTON*/
+
+
+	document.querySelector('.indicadores').appendChild(indicador); /*SELECCION DE CONTENEDOR "indicadores" */
+}
+
