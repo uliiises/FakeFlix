27553697c1806035766317cbadb0ae026e3e1651
UlisesPA <ulises-96_@hotmail.com>
2020-07-26 01:36:10 -0500

    HOVER DE IMAGENES DE CAROUSEL

--- js/main.js
+++ js/main.js
@@ -7,13 +7,26 @@ const flecha_der = document.getElementById('flecha_der');
 /*------------------- EVENT LISTENER PARA FLECHA DERECHA -------------------*/
 
 flecha_der.addEventListener('click', () => {
-	fila.scrollLeft += fila.offsetWidth
+	fila.scrollLeft += fila.offsetWidth;
+
+	/*ASIGNACION DE INDICADOR ACTIVO EN BOTONES DE CAROUSEL AL PRESIONAR FLECHA DERECHA*/
+	const indicadorActivo = document.querySelector('.indicadores .activo');
+	if (indicadorActivo.nextSibling) { /*CONDICION DE ELEMENTO A LA DERECHA EN EL INDICADOR ACTIVO*/
+		indicadorActivo.nextSibling.classList.add('activo'); /*ACTIVAR EL SIGUIENTE BOTON*/
+		indicadorActivo.classList.remove('activo'); /*DESACTIVAR BOTON ANTERIOR*/
+	}
 });
 
 /*------------------- EVENT LISTENER PARA FLECHA IZQUIERDA -------------------*/
 
 flecha_izq.addEventListener('click', () => {
-	fila.scrollLeft -= fila.offsetWidth
+	fila.scrollLeft -= fila.offsetWidth;
+
+	const indicadorActivo = document.querySelector('.indicadores .activo');
+	if (indicadorActivo.previousSibling) { /*CONDICION DE ELEMENTO A LA DERECHA EN EL INDICADOR ACTIVO*/
+		indicadorActivo.previousSibling.classList.add('activo'); /*ACTIVAR EL SIGUIENTE BOTON*/
+		indicadorActivo.classList.remove('activo'); /*DESACTIVAR BOTON ANTERIOR*/
+	}
 });
 
 /*------------------- PAGINACION -------------------*/
@@ -25,8 +38,33 @@ for (let i = 0; i < numeroPaginas; i++) { /*CONDICION DE NUMERO DE PAGINAS*/
 	if (i=== 0) { /*CONDICIONAL DE INDICADOR ACTIVO DE CSS*/
 		indicador.classList.add('activo');
 	}
-	document.querySelector('.indicadores').appendChild(indicador); /*SELECCION DE CONTENEDOR "indicadores" */
-	indicador.addEventListener('click', (e) =>{
-		fila.scrollLeft = i * fila.offsetWidth;
-	})
+	document.querySelector('.indicadores').appendChild(indicador); /*ASIGNACION DE CONTENEDOR "indicadores" */
+	/*ASIGNACION DE EVENTO DE CLICK*/
+	indicador.addEventListener('click', (e) => {
+		fila.scrollLeft = i * fila.offsetWidth; /*CAMBIO DE PAGINA DE CAROUSEL*/
+
+		/*BOTON DE INDICADOR ACTIVO AL PRESIONAR EL BOTON SUPERIOR DERECHO DE PAGINA*/
+		document.querySelector('.indicadores .activo').classList.remove('activo');
+		e.target.classList.add('activo');
+	});
 }
+
+/*------------------- HOVER -------------------*/
+peliculas.forEach((pelicula) => {
+	/*Se selecciona cada pelicula para que al momento de pasar el cursor obtenga el elemento*/
+	pelicula.addEventListener('mouseenter', (e) => {
+		const elemento = e.currentTarget;
+		setTimeout(() => {
+			/*Buscar todas las peliculas para eliminar el hover que no está seleccionado*/
+			peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
+			elemento.classList.add('hover');
+		}, 300);
+	});
+});
+
+/*EVENTO PARA QUITAR LA SELECCION A LA IMAGEN AL QUITAR EL MOUSE*/
+fila.addEventListener('mouseleave', () => {
+	peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
+});
+
+/*tutorial: https://www.youtube.com/watch?v=1ukG4FTmdWQ */
